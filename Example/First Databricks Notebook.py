a248b2acff2226f945f551667691b3a13eef19cd
hassanashas <hassanashas>
2023-07-14 19:23:32 +0000

    Adding a Function notebook

--- Example/First Databricks Notebook.py
+++ Example/First Databricks Notebook.py
@@ -1,2 +1,21 @@
 # Databricks notebook source
 print("Damn man, already starting this!")
+
+# COMMAND ----------
+
+# MAGIC %run ./SecondFunctionNotebook
+
+# COMMAND ----------
+
+print(full_name)
+
+# COMMAND ----------
+
+# MAGIC %md # This is my First Header
+# MAGIC
+# MAGIC - First bullet
+# MAGIC - You see how created a damn function up there??
+
+# COMMAND ----------
+
+
